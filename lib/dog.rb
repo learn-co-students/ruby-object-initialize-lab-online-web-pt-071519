8a559436d4b0136c1d326d71e764a56588d283c3
assane savadogo <assane.savadogo81@gmail.com>
2019-07-29 14:04:06 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,6 @@
 class Dog 
   def initialize(name, breed = "Mutt")
     @name = name 
-    @breed = breed 
-  end
-  
-  def name=(name, breed = "Mutt")
-    @name = name 
-    @breed = breed 
-  end 
-  
-  def name 
-    @name 
-  end 
-  
-  def breed
-    @breed 
+    @breed = breed
   end
 end
