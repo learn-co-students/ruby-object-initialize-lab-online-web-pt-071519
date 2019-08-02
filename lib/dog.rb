179a992276022a28b5cfdc14da44f836e863b1ca
Colleen Halpenny <cp.halpenny@gmail.com>
2019-08-02 01:55:30 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,7 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
-    if @breed = breed
-      return @breed
-    else
-      "Mutt"
-    end
+    @breed = breed
   end
 
   def name=(name)
